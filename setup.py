e2f2ba6a5f669b38bd2b9e2f817cd42a1142d0dd
Mingde Yin <mingdeyin@gmail.com>
2022-08-14 22:43:20 -0400

    packaging and distribution setup

--- setup.py
+++ setup.py
@@ -1,9 +1,27 @@
 from setuptools import setup, find_packages
 
-setup(name='hypernova', version='0.1.0', packages=find_packages(),
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='hypernova-orbit',
+      version='0.1.0',
+      author="Mingde Yin",
+      description="Orbit propagation and analysis tool.",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/spacesys-finch/hypernova/tree/main/hypernova",
+      license="BSD-3-Clause",
+      packages=find_packages(),
       setup_requires=["cffi>=1.15.0"],
       cffi_modules=["hypernova/build_c_code.py:ffibuilder"],
       install_requires=["cffi>=1.15.0",
                         "numpy",
                         "matplotlib"],
-      options={'build_ext': {'inplace': True}})
+      options={'build_ext': {'inplace': True}},
+      classifiers=["Programming Language :: Python :: 3",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: C",
+                   "Topic :: Scientific/Engineering :: Physics",
+                   "Intended Audience :: Science/Research"],
+      python_requires='>=3.6')
